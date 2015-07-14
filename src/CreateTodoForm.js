77b891bafe0d2e07a75f189ba777e4668e5b8351
Scott Burch <scott@bulldoginfo.com>
2015-07-13 23:45:51 -0700

    clear input field for new todo

--- src/CreateTodoForm.js
+++ src/CreateTodoForm.js
@@ -3,15 +3,20 @@ Component = require('react').Component;
 var nextId = 0;
 
 module.exports = class CreateTodoForm extends Component {
+    constructor() {
+        this.state = {nextId: 0};
+    }
+
     createTodo() {
         var description = this.refs.description.value;
         description && RS.set('todos', RS.get('todos').concat({id: nextId++, description: description}));
+        this.refs.description.value = "";
     }
 
     render() {
         return (
             <div>
-                <div>Todo description: <input ref="description" /></div>
+                <div>Todo description: <input defaultValue="" ref="description" /></div>
                 <div><button onClick={this.createTodo.bind(this)}>Create todo</button></div>
             </div>
         )

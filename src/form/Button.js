745f4c8530db3a4c361e452983a181474cab094b
Scott Burch <scott@bulldoginfo.com>
2015-07-19 17:45:46 -0700

    added video files

--- src/form/Button.js
+++ src/form/Button.js
@@ -1,9 +1,18 @@
 var Component = require('../Component');
+var _ = require('lodash');
 
 module.exports = class Button extends Component {
+    setBold(bold) {
+        setTimeout(() => this.mounted && this.setState({bold: bold}));
+    }
+
     render() {
+        var props = _.extend({},this.props);
+        props.style = _.extend({}, props.style, {fontWeight: this.state && this.state.bold ? 'bold' : ''});
         return (
-            <button {...this.props}>{this.props.children}</button>
+            <button {...props} onMouseUp={this.setBold.bind(this, false)} onMouseDown={this.setBold.bind(this, true)}>
+                {this.props.children}
+            </button>
         )
     }
 };

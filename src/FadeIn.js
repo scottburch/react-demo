56617b7e2a3543290d4bb1d62c6e3eb79b7cd556
Scott Burch <scott@bulldoginfo.com>
2015-12-18 09:50:16 -0800

    small cleanups

--- src/FadeIn.js
+++ src/FadeIn.js
@@ -1,7 +1,7 @@
-var Component = require('Component');
+var PureRenderComponent = require('PureRenderComponent');
 var $j = require('jquery');
 
-module.exports = class FadeIn extends Component {
+module.exports = class FadeIn extends PureRenderComponent {
     componentDidMount() {
         $j(this.refs.wrapper).fadeIn(this.props.duration === undefined ? 1000 : this.props.duration);
     }
